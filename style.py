fea844783f1cd22acc04df1220899d7907038cf0
Carlos Arias <cararias@gmail.com>
2024-07-28 21:54:53 +0200

    add blue style

--- style.py
+++ style.py
@@ -3,7 +3,7 @@ from nicegui import ui
 ui.add_head_html("""
 <style>
 .custom-upload .q-uploader__header {
-    background-color: teal; /* Your desired background color */
+    background-color: #3f51b5; /* Your desired background color */
     color: white; /* Optional: change text color */
 }
 </style>
