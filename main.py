d584ee93da5ed8af68c47e41b1cfdd6f4c9a404b
Carlos Arias <cararias@gmail.com>
2024-07-28 23:26:12 +0200

    add word reviewer ui and logic

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ from style import  *
 
 uploaded_file_name = None
 file_content = None
-latest_code = ''
+
+def restart():
+    global uploaded_file_name, file_content
+    file_content = None
+    uploaded_file_name = None
+    generate_button.set_text('Review Text File!')
+    generate_button.disable()
+    source_row.visible = False
+    show_source_row.visible = False 
+    results_row.visible = False
+    restart_button.visible = False 
+    # ui.navigate.reload()
 
 async def check_connection():
     (success, result) = await check_backend_connection()
@@ -33,77 +44,57 @@ async def handle_button_click():
         return
     generate_button.set_text('Loading...')
     generate_button.disable()
+    restart_button.visible = True
     (success, result) = await call_assistant_api(uploaded_file_name, file_content)
     if success:
-        # ui.notify(f'API call successful!')
         generate_button.set_text('Response bellow')
-        indented_result = json.dumps(result, indent=4)
-        json_output.set_value(value=indented_result)
-        diagram_extracted_code = extract_mermaid_code(result[0]['content'][0]['text']['value'])
-        extracted_code.set_value(diagram_extracted_code)
-        diagram.content=diagram_extracted_code
-        json_output.update()
-        extracted_code.update()
-        diagram.update()
-        panels.visible = True
-        tabs.visible = True
+        reviewed_text.set_value(extract_reviewed_text(result[0]['content'][0]['text']['value']))
+        list_corrections.set_value(extract_corrections(result[0]['content'][0]['text']['value']))
+        source_text.set_value(result[0]['content'][0]['text']['value'])
+        reviewed_text.update()
+        list_corrections.update()
+        source_text.update()
+        show_source_row.visible = True
+        results_row.visible = True
 
     else:
         ui.notify(f'API call failed: {result}', type='negative')
 
-def on_code_change(event):
-    global latest_code
-    try:
-        latest_code = event.value
-        if diagram.visible:
-            diagram.content = latest_code
-            diagram.update()
-    except Exception as e:
-        print(f"Error updating diagram: {e}")
-
-def on_tab_change(event):
-    global latest_code
-    if event.name == 'show' and event.args[0] == 'diagram':
-        try:
-            diagram.content = latest_code
-            diagram.update()
-        except Exception as e:
-            print(f"Error updating diagram: {e}")
-
-
+def show_source():
+    source_row.visible = not source_row.visible
+    
 dark = ui.dark_mode()
 dark.enable()
 
 with ui.column().classes('w-full  items-center '):
     with ui.row().classes('flex '):
-        ui.label('Code 2 Diagram').classes('text-2xl')
-    # with ui.row():
-    #     ui.codemirror(language='JSON', theme='darcula').classes('h-96 w-full').set_value(contentDemo)
+        ui.label('Word reviewer').classes('text-2xl')
     with ui.row():    
-        with ui.chip('Backend connection ', icon='question', on_click=check_connection).classes('bg-teal hover:bg-green text-white font-bold py-2 px-4 rounded'):
-            ui.tooltip('Click to check your connection to FastAPI').classes('bg-teal-900')
-            badge = ui.badge('?', color='teal-900').classes('text-white ml-2')
+        with ui.chip('Backend connection ', icon='question', on_click=check_connection).classes('bg-indigo hover:bg-green text-white font-bold py-2 px-4 rounded'):
+            ui.tooltip('Click to check your connection to FastAPI').classes('bg-indigo-900')
+            badge = ui.badge('?', color='indigo-900').classes('text-white ml-2')
     ui.upload(on_upload=handle_upload).classes('custom-upload  text-white font-bold py-2 px-4 rounded') 
     with ui.row():    
-        generate_button = ui.button('Generate Diagram!', 
-            on_click=lambda: asyncio.create_task(handle_button_click())).classes('bg-teal hover:bg-green text-white font-bold py-2 px-4 rounded')
+        restart_button = ui.button('Restart', on_click=restart).classes('bg-pink-9 hover:bg-green text-white font-bold py-2 px-4 rounded')
+        restart_button.visible = False 
+        generate_button = ui.button('Review Text File!', 
+            on_click=lambda: asyncio.create_task(handle_button_click())).classes('bg-indigo hover:bg-green text-white font-bold py-2 px-4 rounded')
         generate_button.disable()
-    with ui.tabs().classes('w-full') as tabs:
-        code_editor = ui.tab('Raw response')
-        extracted_code = ui.tab('Diagram code')
-        diagram = ui.tab('Diagram')
-        tabs.visible = False
-    with ui.tab_panels(tabs, value=diagram).classes('w-full') as panels:
-        panels.on('show', on_tab_change)
-        with ui.tab_panel(code_editor):
-            json_output = ui.codemirror(language='JSON', theme='darcula').classes('h-96 w-full')
-        with ui.tab_panel(extracted_code):
-            extracted_code = ui.codemirror(language='JSON', theme='darcula').classes('h-96 w-full').on_value_change(on_code_change)
-        with ui.tab_panel(diagram).classes('items-center'):
-            diagram = ui.mermaid(content='').on('error', lambda e: print(e.args['message'])).classes('h-96 w-full')
-            
-            # ui.label('File upload')
-        panels.visible = False
+    with ui.row().classes('w-full flex-wrap justify-evenly') as results_row:
+        with ui.column().classes('w-full col-12 col-md-5  items-center '):
+            ui.label('Reviewed Text').classes('text-xl')
+            reviewed_text = ui.codemirror(theme='darcula', line_wrapping=True).classes('h-96 w-full')
+        with ui.column().classes('w-full col-12 col-md-5  items-center '):
+            ui.label('List of Corrections').classes('text-xl')
+            list_corrections = ui.codemirror(theme='darcula', line_wrapping=True).classes('h-96 w-full')
+        results_row.visible = False
+    with ui.row() as show_source_row:
+        show_source_button = ui.button('Show OpenAI Assistant response', on_click=show_source).classes('bg-indigo hover:bg-green text-white font-bold py-2 px-4 rounded')
+        show_source_row.visible = False 
+    with ui.row().classes('w-full no-wrap justify-evenly') as source_row:
+        with ui.column().classes('w-full  items-center '):
+            source_text = ui.codemirror(theme='darcula', line_wrapping=True).classes('h-96 w-full')
+        source_row.visible = False
 
 
 ui.run()
